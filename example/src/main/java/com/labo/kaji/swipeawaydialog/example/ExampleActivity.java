976b21e59f34c28f8967ff91376aaba23872e3a2
kaji <kajiwara@infocity.co.jp>
2015-08-15 15:06:11 +0900

    Initial commit

---
+++ example/src/main/java/com/labo/kaji/swipeawaydialog/example/ExampleActivity.java
@@ -0,0 +1,31 @@
+package com.labo.kaji.swipeawaydialog.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_example, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
