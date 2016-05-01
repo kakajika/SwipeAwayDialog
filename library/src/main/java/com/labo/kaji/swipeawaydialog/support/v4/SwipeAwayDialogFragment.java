ddc0137ee2892c61603200881f24bd124257e31f
kaji <kajiwara@infocity.co.jp>
2016-05-01 16:03:14 +0900

    Support both v4 and v14 Fragments

---
+++ library/src/main/java/com/labo/kaji/swipeawaydialog/support/v4/SwipeAwayDialogFragment.java
@@ -0,0 +1,96 @@
+package com.labo.kaji.swipeawaydialog.support.v4;
+
+import android.support.v4.app.DialogFragment;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import com.labo.kaji.swipeawaydialog.SwipeDismissTouchListener;
+import com.labo.kaji.swipeawaydialog.SwipeableFrameLayout;
+
+/**
+ * @author kakajika
+ * @since 15/08/15.
+ */
+public class SwipeAwayDialogFragment extends DialogFragment {
+
+    private boolean mSwipeable = true;
+    private boolean mTiltEnabled = true;
+    private boolean mSwipeLayoutGenerated = false;
+    private SwipeDismissTouchListener mListener = null;
+
+    /**
+     * Set whether dialog can be swiped away.
+     */
+    public void setSwipeable(boolean swipeable) {
+        mSwipeable = swipeable;
+    }
+
+    /**
+     * Get whether dialog can be swiped away.
+     */
+    public boolean isSwipeable() {
+        return mSwipeable;
+    }
+
+    /**
+     * Set whether tilt effect is enabled on swiping.
+     */
+    public void setTiltEnabled(boolean tiltEnabled) {
+        mTiltEnabled = tiltEnabled;
+        if (mListener != null) {
+            mListener.setTiltEnabled(tiltEnabled);
+        }
+    }
+
+    /**
+     * Get whether tilt effect is enabled on swiping.
+     */
+    public boolean isTiltEnabled() {
+        return mTiltEnabled;
+    }
+
+    /**
+     * Called when dialog is swiped away to dismiss.
+     * @return true to prevent dismissing
+     */
+    public boolean onSwipedAway() {
+        return false;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (!mSwipeLayoutGenerated && getShowsDialog()) {
+            Window window = getDialog().getWindow();
+            ViewGroup decorView = (ViewGroup)window.getDecorView();
+            View content = decorView.getChildAt(0);
+            decorView.removeView(content);
+
+            SwipeableFrameLayout layout = new SwipeableFrameLayout(getActivity());
+            layout.addView(content);
+            decorView.addView(layout);
+
+            mListener = new SwipeDismissTouchListener(decorView, "layout", new SwipeDismissTouchListener.DismissCallbacks() {
+                @Override
+                public boolean canDismiss(Object token) {
+                    return isCancelable() && mSwipeable;
+                }
+
+                @Override
+                public void onDismiss(View view, Object token) {
+                    if (!onSwipedAway()) {
+                        dismiss();
+                    }
+                }
+            });
+            mListener.setTiltEnabled(mTiltEnabled);
+            layout.setSwipeDismissTouchListener(mListener);
+            layout.setOnTouchListener(mListener);
+            layout.setClickable(true);
+            mSwipeLayoutGenerated = true;
+        }
+    }
+
+}
